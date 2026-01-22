90cea82a9dfbe49611373ccfe3582f0eb5b78eb3
김지성 <js.kim@eunwoosoft.com>
2026-01-22 12:40:51 +0900

    update: 메뉴 스타일 및 스크립트 수정

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,45 +1,65 @@
 
 // Click event
 document.addEventListener("click", (e) => {
+	const el = e.target;
 
 	// 'menu-all'
-	if (e.target.closest(".menu-all")) {
-		const clickMenuAll = e.target.closest(".menu-all");
-
-		const menuSubjectList = document.querySelectorAll(".menu-subject");
-		menuSubjectList.forEach((el) => {
-			el.classList.remove("active");
-			if (el.nextElementSibling) {
-				el.nextElementSibling.style.display = "none";
-			}
-		});
-
-		clickMenuAll.classList.add("active");
+	if (el.closest(".menu-all")) {
+		const clickMenuAll = el.closest(".menu-all");
+		menuClickEvent.menuAll(clickMenuAll);
 	}
 
 	// 'menu-subject'
-	if (e.target.closest(".menu-subject")) {
-		const clickMenuSubject = e.target.closest(".menu-subject");
+	if (el.closest(".menu-subject")) {
+		const clickMenuSubject = el.closest(".menu-subject");
+		menuClickEvent.menuSubject(clickMenuSubject);
+	}
+
+	// 'menu-item'
+	if (el.closest(".menu-item")) {
+		const clickMenuItem = el.closest(".menu-item");
+		menuClickEvent.menuItem(clickMenuItem);
+	}
+});
+
+const menuClickEvent = {
+	menuAll: (clickMenuAll) => {
+		const menuItemList = document.querySelectorAll(".menu-item");
+		menuItemList.forEach((menuItem) => {
+			menuItem.classList.remove("active");
+		});
+
+		clickMenuAll.classList.add("active");
+	},
+
+	menuSubject: (clickMenuSubject) => {
 		const isActive = clickMenuSubject.classList.contains("active");
 
-		const clickMenuAll = document.querySelector(".menu-all");
-		clickMenuAll.classList.remove("active");
-
-		const menuSubjectList = document.querySelectorAll(".menu-subject");
-		menuSubjectList.forEach((el) => {
-			el.classList.remove("active");
-			if (el.nextElementSibling) {
-				el.nextElementSibling.style.display = "none";
-			}
-		});
-		
 		if (!isActive) {
 			clickMenuSubject.classList.add("active");
+			clickMenuSubject.nextElementSibling.classList.add("visible");
+			clickMenuSubject.nextElementSibling.classList.remove("invisible");
 			clickMenuSubject.nextElementSibling.style.display = "block";
 
 		} else {
-			clickMenuSubject.nextElementSibling.style.display = "none";
+			clickMenuSubject.classList.remove("active");
+			clickMenuSubject.nextElementSibling.classList.remove("visible");
+			clickMenuSubject.nextElementSibling.classList.add("invisible");
+			setTimeout(() => {
+				clickMenuSubject.nextElementSibling.style.display = "none";
+			}, 200);
 		}
-	}
+	},
 
-});
+	menuItem: (clickMenuItem) => {
+		const clickMenuAll = document.querySelector(".menu-all");
+		clickMenuAll.classList.remove("active");
+
+		const menuItemList = document.querySelectorAll(".menu-item");
+		menuItemList.forEach((menuItem) => {
+			menuItem.classList.remove("active");
+		});
+
+		clickMenuItem.classList.add("active");
+	},
+}
